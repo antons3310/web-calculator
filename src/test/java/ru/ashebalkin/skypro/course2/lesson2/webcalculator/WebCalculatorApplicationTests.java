5a6bb79d926e991ea6a87fc3a55c499be3521a0c
antoshka <ashebalkin@diasoft.ru>
2021-11-29 22:47:06 +0300

    Заготовка для сдачи ДЗ

---
+++ src/test/java/ru/ashebalkin/skypro/course2/lesson2/webcalculator/WebCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ashebalkin.skypro.course2.lesson2.webcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
